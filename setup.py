89554e129b10d2269e574053f409c699b7a3119b
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-04-24 15:34:00 -0500

    Fix dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ kwargs = {'author': 'Adam Nagel',
           'download_url': '',
           'entry_points': '[openmdao.component]\nfmu_wrapper.FmuWrapper=fmu_wrapper:FmuWrapper\n\n[openmdao.container]\nfmu_wrapper.FmuWrapper=fmu_wrapper:FmuWrapper',
           'include_package_data': True,
-          'install_requires': ['openmdao', 'pyfmi', 'Assimulo'],
+          'install_requires': ['openmdao', 'pyfmi', 'Assimulo', 'matplotlib', 'lxml'],
           'keywords': ['openmdao, fmu'],
           'license': 'GNU General Public License, version 2',
           'maintainer': 'Adam Nagel',
@@ -18,7 +18,7 @@ kwargs = {'author': 'Adam Nagel',
           'package_dir': {'fmu_wrapper': 'fmu_wrapper'},
           'package_data': {'fmu_wrapper.test': ['test/bouncingBall.fmu']},
           'url': 'https://github.com/metamorph-inc/fmu_wrapper',
-          'version': '0.2',
+          'version': '0.3',
           'zip_safe': False}
 
 setup(**kwargs)
